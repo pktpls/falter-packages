622ba16c00d13fc7d94a15e0f19065f0b7f1d893
Packet Please <pktpls@systemli.org>
2026-08-08 02:51:17 +0200

    falter-web: hide password absence warning
    Signed-off-by: Packet Please <pktpls@systemli.org>

--- packages/falter-web/root/www/luci-static/resources/view/falter/index2.js
+++ packages/falter-web/root/www/luci-static/resources/view/falter/index2.js
@@ -8,7 +8,16 @@ const callFalterStatus = rpc.declare({
   expect: { '': {} }
 });
 
+// Detect whether a password has been set for the root account.
+// Do it the easy way by checking if Luci rendered the warning alert.
+// Our CSS hides this alert from our users so they use our own password flow.
+function isPasswordAbsent() {
+  const sel = '.warning:has(a.btn[href*="/admin/system/admin"])';
+  return document.querySelectorAll(sel).length > 0;
+}
+
 return view.extend({
+  passwordAbsent: isPasswordAbsent(),
 
   load() {
     return L.resolveDefault(callFalterStatus());
@@ -18,6 +27,7 @@ return view.extend({
     return E([], [
       E('h1', {}, _('Hello, world!')),
       E('pre', {}, `status: ${falterStatus.status}`),
+      E('pre', {}, `passwordAbsent: ${(this.passwordAbsent)}`)
     ]);
   },
 
